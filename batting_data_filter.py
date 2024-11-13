8169adb43dad586f4f7414b07838937df4ef1134
grskrut <skrut5921@utexas.edu>
2024-11-12 21:19:19 -0600

    comment update

--- batting_data_filter.py
+++ batting_data_filter.py
@@ -7,17 +7,15 @@ Created on Mon Nov 11 19:22:46 2024
 
 import pandas as pd
 
-# Load your CSV file into a DataFrame
+# load csv file
 df = pd.read_csv('2023batting.csv')
 
-# Specify the columns you want to keep
+# keep wanted data columns
 columns_to_keep = ['gid', 'id', 'team', 'b_pa', 'b_ab', 'b_h', 'b_d', 'b_t', 'b_hr', 'b_rbi', 'b_w', 'b_k', 'date', 'wl']  # Replace with your column names
 
-# Create a new DataFrame with only the selected columns
+# new data frame
 df_filtered = df[columns_to_keep]
 
-# Optionally, save the filtered DataFrame to a new CSV
+# save new csv file
 df_filtered.to_csv('2023_filtered_batting_data.csv', index=False)
 
-# Print the filtered DataFrame to verify
-#print(df_filtered)
--- pitching_data_filter.py
+++ pitching_data_filter.py
@@ -7,17 +7,15 @@ Created on Mon Nov 11 19:22:46 2024
 
 import pandas as pd
 
-# Load your CSV file into a DataFrame
+# load csv file
 df = pd.read_csv('2023pitching.csv')
 
-# Specify the columns you want to keep
+# keep wanted data columns
 columns_to_keep = ['gid', 'id', 'team', 'p_seq', 'p_h', 'p_r', 'p_er', 'p_w', 'p_hbp', 'p_wp', 'date', 'wl']  # Replace with your column names
 
-# Create a new DataFrame with only the selected columns
+# new data frame
 df_filtered = df[columns_to_keep]
 
-# Optionally, save the filtered DataFrame to a new CSV
+# save filtered csv file
 df_filtered.to_csv('2023_filtered_pitching_data.csv', index=False)
 
-# Print the filtered DataFrame to verify
-#print(df_filtered)
